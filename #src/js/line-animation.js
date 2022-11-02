98462c030fd29c5659bffdbde23c26990456fd44
Maksim-1307 <max.07mal@gmail.com>
2022-11-02 21:56:48 +0300

    line section done

--- #src/js/line-animation.js
+++ #src/js/line-animation.js
@@ -7,9 +7,3 @@ window.addEventListener('scroll', function () {
     textpath.setAttribute('startOffset', scrollSelf - document.documentElement.scrollTop);
     document.querySelector(".line-section").style.bottom = -(document.documentElement.scrollTop - (scrollSelf - 800)) / 5 + 'px';
 });
-
-var r = document.querySelector(".top-section__images-container");
-
-window.addEventListener('scroll', function () {
-  r.style.setProperty('--scroll-bias', document.documentElement.scrollTop / 3 + 'px');
-});
--- #src/js/top-section-animation.js
+++ #src/js/top-section-animation.js
@@ -1,14 +1,5 @@
-
 var r = document.querySelector(".top-section__images-container");
 
-
-console.log(document.documentElement.scrollTop)
-
-var rs = getComputedStyle(r);
-
-let startScroll = document.documentElement.scrollTop;
 window.addEventListener('scroll', function () {
-  let startScroll = document.documentElement.scrollTop;
-  console.log(document.documentElement.scrollTop);
-  r.style.setProperty('--scroll-bias', startScroll / 3 + 'px');
+  r.style.setProperty('--scroll-bias', document.documentElement.scrollTop / 3 + 'px');
 });
